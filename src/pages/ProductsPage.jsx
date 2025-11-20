8e5f3f0396666d495484865afd8dca5fd5685ab8
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-11-20 19:14:07 +0330

    Add data via api

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,7 @@
+import { useProducts } from "../context/ProductContext";
 function ProductsPage() {
+  const products = useProducts();
+  console.log(products);
   return <div>ProductsPage</div>;
 }
 
