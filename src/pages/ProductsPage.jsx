718b1cf1a40d64c2524e33105784a0ad8bf0fe34
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-11-21 11:28:59 +0330

    add styles products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,14 +1,15 @@
 import { useProducts } from "../context/ProductContext";
 import styles from "./ProductsPage.module.css";
+import Card from "../components/Card";
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
+
   return (
     <div className={styles.container}>
       <div className={styles.products}>
         {!products.length && <p>Loading...</p>}
         {products.map((p) => (
-          <p key={p.id}>{p.title}</p>
+          <Card key={p.id} data={p} />
         ))}
       </div>
       <div>sidebar</div>
