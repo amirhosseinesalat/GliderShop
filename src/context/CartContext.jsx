e27171edc53e979c967900b1734977ed7e72b96b
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-11-27 09:24:51 +0330

    add complated reducer

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -13,10 +13,39 @@ const reducer = (state, action) => {
         state.selectedItems.push({ ...action.payload, quantity: 1 });
       }
       return {
-        selectedItems: [...state.selectedItems],
+        ...state,
         ...sumProducts(state.selectedItems),
         checkout: false,
       };
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+    case "INCREASE":
+      const increaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[increaseIndex].quantity++;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    case "DECREASE":
+      const decreaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[decreaseIndex].quantity--;
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+      };
+    case "CHECKOUT":
+      return { selectedItems: [], itemsCounter: 0, total: 0, checkout: true };
     default:
       throw new Error("Invalid action");
   }
