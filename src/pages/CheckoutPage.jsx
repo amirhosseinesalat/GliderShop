a72358a58633ab102e3e0e5dcfa1f883cc300ce2
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-11-28 17:21:38 +0330

    added basketcard with full design

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,16 @@
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartContext";
 function CheckoutPage() {
-  return <div>Checkout</div>;
+  const [state, dispatch] = useCart();
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
+  return (
+    <div>
+      {state.selectedItems.map((p) => (
+        <BasketCard id={p.id} data={p} clickHandler={clickHandler} />
+      ))}
+    </div>
+  );
 }
 
 export default CheckoutPage;
